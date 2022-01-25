b49d1d3cbe63381826740961f3324ee63a325b54
Stephanie Westerhuis <stephanie.westerhuis@usys.ethz.ch>
2022-01-25 14:54:29 +0100

    implemented retrieve COSMO

--- src/fls_sat_verif/cli.py
+++ src/fls_sat_verif/cli.py
@@ -1,13 +1,16 @@
 """Command line interface of fls_sat_verif."""
 # Standard library
 import logging
+from email.policy import default
 
 # Third-party
 import click
 
 # Local
 from . import __version__
+from .utils import calc_fls_fractions
 from .utils import count_to_log_level
+from .utils import retrieve_cosmo_files
 
 
 @click.command()
@@ -30,12 +33,69 @@ from .utils import count_to_log_level
     is_flag=True,
     help="Print version",
 )
-def main(*, dry_run: bool, verbose: int, version: bool) -> None:
+@click.option(
+    "--retrieve_cosmo",
+    is_flag=True,
+    help="Retrieve cosmo forecast files from store from <start> to <end>.",
+)
+@click.option(
+    "--calc_fractions", is_flag=True, help="Calculate FLS fractions from OBS and FCST."
+)
+@click.option(
+    "--start",
+    type=click.DateTime(formats=["%y%m%d%H"]),
+    help="Start date: YYMMDDHH.",
+)
+@click.option(
+    "--end",
+    type=click.DateTime(formats=["%y%m%d%H"]),
+    help="End date: YYMMDDHH.",
+)
+@click.option("--init", type=int, multiple=True, help="Init time, e.g. 00 UTC, 12 UTC.")
+@click.option("--interval", type=int, help="Time between init of simulations.")
+@click.option(
+    "--max_lt", type=int, default=33, help="Maximal leadtime in hours. Default: 33"
+)
+@click.option(
+    "--model_dir",
+    type=str,
+    help="Directory with TQC from model output.",
+    default="/scratch/swester/input_sat_verif/tqc",
+)
+@click.option(
+    "--obs_dir",
+    type=str,
+    help="Directory with raw low cloud confidence level files.",
+    default="/scratch/swester/input_sat_verif/sat",
+)
+@click.option(
+    "--fls_dir",
+    type=str,
+    help="Directory where pickled dataframes containing FLS fractions are stored.",
+    default="/scratch/swester/input_sat_verif/fls",
+)
+def main(
+    *,
+    dry_run: bool,
+    verbose: int,
+    version: bool,
+    retrieve_cosmo: bool,
+    calc_fractions: bool,
+    start: str,
+    end: str,
+    init: int,
+    interval: int,
+    max_lt: int,
+    model_dir: str,
+    obs_dir: str,
+    fls_dir: str,
+) -> None:
+
     logging.basicConfig(level=count_to_log_level(verbose))
 
-    logging.warning("This is a warning.")
-    logging.info("This is an info message.")
-    logging.debug("This is a debug message.")
+    # logging.warning("This is a warning.")
+    # logging.info("This is an info message.")
+    # logging.debug("This is a debug message.")
 
     if version:
         click.echo(__version__)
@@ -45,7 +105,12 @@ def main(*, dry_run: bool, verbose: int, version: bool) -> None:
         click.echo("Is dry run")
         return
 
-    click.echo(
-        "Replace this message by putting your code into test_cli_project.cli.main"
-    )
-    click.echo("See click documentation at http://click.pocoo.org/")
+    if retrieve_cosmo:
+        retrieve_cosmo_files(
+            start=start, end=end, interval=interval, out_dir=model_dir, max_lt=max_lt
+        )
+
+    if calc_fractions:
+        calc_fls_fractions(
+            start, end, in_dir_obs=obs_dir, in_dir_model=model_dir, out_dir_fls=fls_dir
+        )
--- src/fls_sat_verif/utils.py
+++ src/fls_sat_verif/utils.py
@@ -1,6 +1,15 @@
 """Utils for the command line tool."""
 # Standard library
 import logging
+import os
+import pickle
+import sys
+from bdb import set_trace
+from pathlib import Path
+
+# Third-party
+import pandas as pd
+from click import option
 
 
 def count_to_log_level(count: int) -> int:
@@ -13,3 +22,99 @@ def count_to_log_level(count: int) -> int:
         return logging.INFO
     else:
         return logging.DEBUG
+
+
+def extract_tqc(grib_file, out_dir, date_str, lt):
+    """Extract tqc from model file using fieldextra.
+
+    Args:
+        grib_file (str): Grib file
+        out_dir (str): Output directory
+        date_str (str): date
+        lt (int): leadtime
+
+    """
+    logging.debug(f"Apply fxfilter to: {grib_file}.")
+
+    # new filename
+    new_name = Path(out_dir, f"tqc_{date_str}_{lt:03}.grb2")
+    logging.info(f"Creating: {str(new_name)}.")
+
+    # check whether filtered file already exists
+    if new_name.is_file():
+        logging.info(f"  ...exists already!")
+        return
+
+    # apply fxfilter
+    cmd = f"fxfilter -o {new_name} -s TQC {grib_file}"
+    logging.debug(f"Will run: {cmd}")
+    os.system(cmd)
+
+    return
+
+
+def retrieve_cosmo_files(start, end, interval, out_dir, max_lt):
+    """Retrieve COSMO files.
+
+    Args:
+        start (datetime):   start
+        end (datetime):     end
+        interval (int):     interval between simulations
+        out_dir (str):      output directory for tqc-files
+        max_lt (int):       maximum leadtime
+
+    """
+    cosmo_dir = f"/store/s83/osm/COSMO-1E/"  # FCST{start.strftime('%y')}"
+    logging.info(f"Retrieving COSMO-files from {cosmo_dir}")
+
+    # create output directory
+    Path(out_dir).mkdir(parents=True, exist_ok=True)
+
+    # list of ini-dates of simulations
+    dates = pd.date_range(start, end, freq=f"{interval}H")
+
+    # loop over simulations
+    for date in dates:
+
+        # string of date for directories
+        date_str = date.strftime("%y%m%d%H")
+
+        # collect grib files
+        for lt in range(0, max_lt + 1, 1):
+            model_file = list(
+                Path(cosmo_dir, f"FCST{date.strftime('%y')}").glob(
+                    f"{date_str}_???/grib/c1effsurf{lt:03}_000"
+                )
+            )
+            if len(model_file) == 0:
+                logging.warning(f"No file found for {date_str}: +{lt}h.")
+            elif len(model_file) > 1:
+                print(f"Model file description ambiguous.")
+                sys.exit(1)
+            else:
+                # apply fxfilter
+                extract_tqc(model_file[0], out_dir, date_str, lt)
+
+
+def get_fls_fractions(in_dir):
+    """Retrieve dataframe containing FLS fractions.
+
+    Args:
+        in_dir (str): input directory
+
+    """
+    pass
+
+
+def calc_fls_fractions(start, end, in_dir_obs, in_dir_model, out_dir_fls):
+    """Calculate FLS fractions in Swiss Plateau for OBS and FCST.
+
+    Args:
+        start (datetime):       start
+        end (datetime):         end
+        in_dir_obs (str):       dir with sat data
+        in_dir_model (str):     dir with model data
+        out_dir_fls (str):      dir with fls fractions
+
+    """
+    pass
